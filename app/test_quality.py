e4a712b56a6583347cf7ab49b5bfa5afedf6a972
Sebastian Marquez <sebastian.marquez.dev@gmail.com>
2025-09-11 18:00:04 +0200

    Test: clean code that should pass all checks

--- app/test_quality.py
+++
@@ -1,6 +0,0 @@
-def bad_function():
-    x = 1 + 2
-    print(
-        "This line is way too long and should trigger flake8 because it exceeds the maximum line length configured"
-    )
-    return x
